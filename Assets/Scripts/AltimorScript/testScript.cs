6c3dc6b5ac5d9592d9cfdfeb87c9ca9efc3e77d2
Altimors <47299710+Altimors@users.noreply.github.com>
2020-04-08 11:12:58 +0200

    meta ajouté dans le gitignore

--- Assets/Scripts/AltimorScript/testScript.cs
+++ Assets/Scripts/AltimorScript/testScript.cs
@@ -7,7 +7,7 @@ public class testScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world !");
     }
 
     // Update is called once per frame
