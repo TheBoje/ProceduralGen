58afdc012c67049c03f8727485012710b5e45aaa
Altimors <47299710+Altimors@users.noreply.github.com>
2020-04-08 13:33:18 +0200

    ajout de la classe "Room"

---
+++ Assets/Scripts/AltimorScript/Room.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    
+    [SerializeField] private List<Vector3> m_anchorsPoints; // Liste des points d'ancrage
+    private int m_nbAnchorsPoints;                          // Nombre de points d'ancrage
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_nbAnchorsPoints = m_anchorsPoints.Count;
+    }
+
+    // GETTER
+    public List<Vector3> anchorsPoints
+    {
+        get { return m_anchorsPoints; }
+    }
+
+    public int nbAnchorsPoints
+    {
+        get { return m_nbAnchorsPoints; }
+    }
+}
