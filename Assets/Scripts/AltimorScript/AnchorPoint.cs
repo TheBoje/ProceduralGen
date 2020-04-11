88a16e281401348c738d8daaf912b06b76e3a0e4
Altimors <47299710+Altimors@users.noreply.github.com>
2020-04-11 17:10:20 +0200

    Les pièces se placent en fonction de leur ancrage

--- Assets/Scripts/AltimorScript/AnchorPoint.cs
+++ Assets/Scripts/AltimorScript/AnchorPoint.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class AnchorPoint : MonoBehaviour
 {
+
+    public enum side { NORTH, EAST, SOUTH, WEST}
+
+    [SerializeField] side m_anchorSide; // à automatiser par la suite en fonction de la position
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Tourne le parent ou non (Room) en fonction de la face qui lui est présenté WIP
+    public void RotateRoom(side otherAnchors)
     {
-        
+
+        Transform parent = transform.parent;
+        Vector3 rot = new Vector3(0f, 90f, 0f);
+
+        if(m_anchorSide == otherAnchors)
+        {
+            parent.Rotate(new Vector3(0f, 180f, 0f));
+            m_anchorSide = (side)(((int)m_anchorSide + 2) % 4);
+        }
+        else
+        {
+            float multiplier = 0.0f;
+
+            if (((int)m_anchorSide - 1) % 4 == (int)otherAnchors) // vérifie le côté gauche
+            {
+                multiplier = -1f;
+                m_anchorSide = (side)(4 + ((int)m_anchorSide + 1) % 4);
+            }
+            else if (((int)m_anchorSide + 1) % 4 == (int)otherAnchors) // vérifie le côté droit
+            {
+                multiplier = 1f;
+                m_anchorSide = (side)( 4 + ((int)m_anchorSide - 1) % 4);
+            }
+                
+            parent.Rotate(rot * multiplier);
+        }
+    }
+
+    // GETTER
+    public side AnchorSide
+    {
+        get { return m_anchorSide; }
     }
 }
--- Assets/Scripts/AltimorScript/Room.cs
+++ Assets/Scripts/AltimorScript/Room.cs
@@ -15,7 +15,6 @@ public class Room : MonoBehaviour
         //m_anchorsPoints = new List<Transform>();
 
         m_nbAnchorsPoints = m_anchorsPoints.Count;
-        Debug.Log(m_nbAnchorsPoints);
     }
 
     // Place tous les points d'ancrage dans la liste
@@ -37,19 +36,37 @@ public class Room : MonoBehaviour
         }
     }
 
+    public void SetParent(GameObject newParent)
+    {
+        gameObject.transform.parent = newParent.transform;
+
+        //Debug.Log("gameObject's Parent: " + gameObject.transform.parent.name);
+    }
+
     // Ajoute une pièce sur le n-ième point d'ancrage (passé en paramètre)
     public void AddRoom(int indOrigin, int indAdding, GameObject room)
     {
         Room roomScript = room.GetComponent<Room>();
-        Debug.Log(m_nbAnchorsPoints);
-        Debug.Log(roomScript.NbAnchorsPoints);
         if( (indOrigin < 0 || indOrigin > m_nbAnchorsPoints - 1) || (indAdding < 0 || indAdding > roomScript.NbAnchorsPoints) )
         {
             Debug.Log("Room.AddRoom() - Invalid index");
         }
         else
         {
-            Instantiate(room, m_anchorsPoints[indOrigin].position - roomScript.AnchorsPoints[indAdding].position, Quaternion.identity);
+            GameObject addedRoom = Instantiate(room, m_anchorsPoints[indOrigin].position, transform.rotation); //Euler(transform.rotation.x, transform.rotation.y - 90, transform.rotation.z)
+
+            addedRoom.GetComponent<Room>().AnchorsPoints[indAdding].GetComponent<AnchorPoint>().RotateRoom(m_anchorsPoints[indOrigin].GetComponent<AnchorPoint>().AnchorSide);
+
+            Vector3 anchorPos = addedRoom.GetComponent<Room>().AnchorsPoints[indAdding].position;
+            Vector3 addedRoomPos = addedRoom.transform.position;
+
+
+            Vector3 vectDeplacement = new Vector3(addedRoomPos.x - anchorPos.x,
+                                                  addedRoomPos.y - anchorPos.y,
+                                                  addedRoomPos.z - anchorPos.z);
+
+
+            addedRoom.transform.position = addedRoom.transform.position + vectDeplacement;
         }
     }
 
