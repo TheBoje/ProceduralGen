ece032050d9328a3e693fb211d0d5f7ceb4abcbb
Altimors <47299710+Altimors@users.noreply.github.com>
2020-05-07 14:36:47 +0200

    unity plante quand on génère les routes

--- Assets/Scripts/AltimorScript/IntersectionMaker.cs
+++ Assets/Scripts/AltimorScript/IntersectionMaker.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 public class IntersectionMaker : MonoBehaviour
 {
     private List<Intersection> m_intersections;
-    [SerializeField] private GameObject poisson;
-    private Vector3 m_format;
+    private GameObject poisson;
 
 
     // Calcul les points les plus proches et retourne la liste des points
@@ -39,13 +38,13 @@ public class IntersectionMaker : MonoBehaviour
     // Calcul les points les plus proches de chaques points
     private void ComputeNearestPoint(int rows, int cols)
     {
-        for(int x = 0; x < poisson.GetComponent<PoissonSampling>().poissonGrid.Length; x++)
+        for(int x = 0; x < rows; x++)
         {
-            for(int y = 0; y < poisson.GetComponent<PoissonSampling>().poissonGrid.Length; y++)
+            for(int y = 0; y < cols; y++)
             {
                 if(poisson.GetComponent<PoissonSampling>().poissonGrid[x, y] != null)
                 {
-                    int nbNearPoints = UnityEngine.Random.Range(1, 3);
+                    int nbNearPoints = 2; //UnityEngine.Random.Range(1, 3);
                     m_intersections.Add(new Intersection(poisson.GetComponent<PoissonSampling>().poissonGrid[x, y], NearestPoint(x, y, nbNearPoints, rows, cols)));
                 }
             }
@@ -58,8 +57,9 @@ public class IntersectionMaker : MonoBehaviour
         // va lancer la génération
         PoissonSampling poissonScript = poisson.GetComponent<PoissonSampling>();
 
-        //ComputeNearestPoint() // TODO get des lignes et colonnes
-        
+        poissonScript.computePoints();
+        ComputeNearestPoint(poissonScript.getRowSize, poissonScript.getColSize); // TODO get des lignes et colonnes
+       /* 
         foreach(Intersection intersection in m_intersections)
         {
             foreach(Vector3 nPos in intersection.neighbours)
@@ -69,6 +69,12 @@ public class IntersectionMaker : MonoBehaviour
                 road.Init(intersection.position, nPos);
                 road.SetRoad();
             }
-        }
+        }*/
+    }
+
+    private void Start()
+    {
+        poisson = gameObject;
+        ComputeRoad();
     }
 }
