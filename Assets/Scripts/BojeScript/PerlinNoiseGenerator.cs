287ea7f5cdaebf5b9d2cca7f202ee69e9090b297
TheBoje <louis.leenar@gmail.com>
2020-05-26 19:38:23 +0200

    commented perlinNoiseGenerator

--- Assets/Scripts/BojeScript/PerlinNoiseGenerator.cs
+++ Assets/Scripts/BojeScript/PerlinNoiseGenerator.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PerlinNoiseGenerator : MonoBehaviour
 {
+    /// <summary>Retourne un bruit de perlin de taille (width, height) 
+    /// représenté par un tableau de float [0,1]</summary>
     public List<List<float>> perlinNoiseGenerateMap(int width, int height)
     {
+        // Initialisation de la liste de points (un points est un float[0,1])
         List<List<float>> perlinResult = new List<List<float>>();
         for (int i = 0; i < width; i++)
         {
@@ -16,9 +19,9 @@ public class PerlinNoiseGenerator : MonoBehaviour
         }
         return perlinResult;
     }
-
-    /*     public float perlinNoiseGeneratePoint(float x, float y, float width, float height, float scale)
-        {
-            return Mathf.PerlinNoise((float)((x / width) * scale), (float)((y / height) * scale));
-        } */
+    /// <summary>Retourne la valeur du bruit de Perlin en (x, y) dans un tableau de (width, height)*scale (optionnal)</summary>
+    public float perlinNoiseGeneratePoint(float x, float y, float width, float height, float scale = 1)
+    {
+        return Mathf.PerlinNoise((float)((x / width) * scale), (float)((y / height) * scale));
+    }
 }
