00695d1aef2b3f43f5fd67081d31427bba4fa056
Altimors <47299710+Altimors@users.noreply.github.com>
2020-06-03 16:20:10 +0200

    Extrusion + List de WingedEdge

---
+++ Assets/Scripts/AltimorScript/WingedEdgeMap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class WingedEdgeMap : MonoBehaviour
+{
+    public Material mat;
+
+    private void Start()
+    {
+
+        ProBuilderMesh quad = ProBuilderMesh.Create(
+            new Vector3[] {
+                new Vector3(0f, 0f, 0f),
+                new Vector3(1f, 0f, 0f),
+                new Vector3(0f, 1f, 0f),
+                new Vector3(1f, 1f, 0f)
+            },
+            new Face[] { new Face(new int[] { 0, 1, 2, 1, 3, 2 } )
+        });
+
+        quad.Extrude(quad.faces, ExtrudeMethod.FaceNormal, 4f);
+        quad.Refresh();
+
+        quad.ToMesh();
+        quad.GetComponent<MeshRenderer>().material = mat;
+
+        List<WingedEdge> wList = WingedEdge.GetWingedEdges(quad);
+
+        Debug.Log("Faces : " + quad.faceCount);
+        Debug.Log("Edges : " + quad.edgeCount);
+
+        
+    }
+
+}
