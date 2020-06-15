8b6755f349ef318579427712ab054f1faeaefe0e
Altimors <47299710+Altimors@users.noreply.github.com>
2020-06-15 15:18:18 +0200

    La carte s'affiche

--- Assets/Scripts/AltimorScript/HalfEdgesMap.cs
+++ Assets/Scripts/AltimorScript/HalfEdgesMap.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.ProBuilder;
 
 // Créer un triangle isolé
 
@@ -36,18 +37,19 @@ public class HalfEdgesMap : MonoBehaviour
     }
 
     // Dessine une face à l'aide d'un Line renderer
-    private List<int> ComputePointsFace(int firstIndex)
+    private List<int> ComputePointsFace(int firstEdge, List<HalfEdge> printList)
     {
-        int currentIndex = firstIndex;
-        int i = 0;
+        int currentIndex = firstEdge;
         List<int> indexPoints = new List<int>();
-        indexPoints.Add(m_halfEdges[firstIndex].Position);
+
+        indexPoints.Add(m_halfEdges[firstEdge].Position);
 
         do
         {
             currentIndex = m_halfEdges[currentIndex].Next;
             indexPoints.Add(m_halfEdges[currentIndex].Position);
-        } while (currentIndex != firstIndex);
+            printList.Remove(m_halfEdges[currentIndex]);
+        } while (currentIndex != firstEdge);
 
         return indexPoints;
     }
@@ -57,15 +59,29 @@ public class HalfEdgesMap : MonoBehaviour
         LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.widthMultiplier = 0.2f;
+        lineRenderer.positionCount = m_halfEdges.Count;
 
-        List<int> indexPoints = ComputePointsFace(0);
+        List<HalfEdge> copy = new List<HalfEdge>(m_halfEdges);
+        List<List<int>> facesList = new List<List<int>>();
 
-        lineRenderer.positionCount = indexPoints.Count;
-
-        for(int i = 0; i < indexPoints.Count; i++)
+        while(copy.Count > 0)
         {
-            lineRenderer.SetPosition(i, m_positions[indexPoints[i]]);
+            facesList.Add(ComputePointsFace(m_halfEdges.IndexOf(copy[0]), copy));
         }
+
+        int i = 0;
+        int face = 0;
+
+        while (i < lineRenderer.positionCount)
+        {
+            for (int vertex = 0; vertex < facesList[face].Count; vertex++)
+            {
+                lineRenderer.SetPosition(i, m_positions[facesList[face][vertex]]);
+                i++;
+            }
+            face++;
+        }
+        
 
     }
 
@@ -76,20 +92,34 @@ public class HalfEdgesMap : MonoBehaviour
         m_positions.Add(new Vector3(0f, 0f, 0f));
         m_positions.Add(new Vector3(0f, 0f, 10f));
         m_positions.Add(new Vector3(10f, 0f, 5f));
+        m_positions.Add(new Vector3(20f, 0f, 0f));
 
-        HalfEdge dart1 = new HalfEdge(1, 2, 3, 0);
+        // Face 1
+        HalfEdge dart1 = new HalfEdge(1, 2, 3, 0); // 0
         m_halfEdges.Add(dart1);
-        HalfEdge dart2 = new HalfEdge(2, 0, 4, 1);
+        HalfEdge dart2 = new HalfEdge(2, 0, 4, 1); // 1
         m_halfEdges.Add(dart2);
-        HalfEdge dart3 = new HalfEdge(0, 1, 5, 2);
+        HalfEdge dart3 = new HalfEdge(0, 1, 5, 2); // 2
         m_halfEdges.Add(dart3);
 
-        HalfEdge opp1 = new HalfEdge(5, 4, 0, 1);
+        HalfEdge opp1 = new HalfEdge(9, 4, 0, 1);  // 3
         m_halfEdges.Add(opp1);
-        HalfEdge opp2 = new HalfEdge(3, 5, 1, 2);
+        HalfEdge opp2 = new HalfEdge(3, 8, 1, 2);  // 4
         m_halfEdges.Add(opp2);
-        HalfEdge opp3 = new HalfEdge(4, 3, 2, 0);
+        HalfEdge opp3 = new HalfEdge(6, 7, 2, 0);  // 5
         m_halfEdges.Add(opp3);
+
+        // Face 2
+        HalfEdge dart12 = new HalfEdge(7, 5, 8, 2); // 6
+        m_halfEdges.Add(dart12);
+        HalfEdge dart22 = new HalfEdge(5, 6, 9, 3); // 7
+        m_halfEdges.Add(dart22);
+
+        HalfEdge opp12 = new HalfEdge(4, 9, 6, 3);  // 8
+        m_halfEdges.Add(opp12);
+        HalfEdge opp22 = new HalfEdge(8, 3, 7, 0);  // 9
+        m_halfEdges.Add(opp22);
+
 
         DrawMap();
     }
