142e55dff6e3dc6a2c2678eb14757f33b11e4a0f
TheBoje <louis.leenar@gmail.com>
2020-06-20 17:28:31 +0200

    added terrainGenerator file

---
+++ Assets/Scripts/BojeScript/TerrainGenerator.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.Experimental.TerrainAPI;
+using System.Drawing;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    private int terrainWidth;
+    private int terrainLength;
+    private float terrainHeight;
+    private float noiseScale;
+    private Terrain terrainComponent;
+    private PoissonSampling poissonSamplingScript;
+
+    private void Start()
+    {
+        terrainComponent = GetComponent<Terrain>();
+        poissonSamplingScript = (PoissonSampling)GameObject.Find("GenManager").GetComponent<PoissonSampling>();
+    }
+
+    public void generateTerrain()
+    {
+        terrainWidth = poissonSamplingScript.rangeX;
+        terrainLength = poissonSamplingScript.rangeZ;
+        terrainHeight = poissonSamplingScript.scaleY;
+        noiseScale = poissonSamplingScript.perlinScale;
+        terrainComponent.terrainData = generateTerrainData(terrainComponent.terrainData);
+    }
+
+    private TerrainData generateTerrainData(TerrainData oldTerrainData)
+    {
+        TerrainData terrainData = oldTerrainData;
+        terrainData.heightmapResolution = Mathf.Max(terrainWidth, terrainLength) + 1;
+        terrainData.size = new Vector3(terrainWidth, terrainHeight, terrainLength);
+        terrainData.SetHeights(0, 0, generateHeights());
+        return terrainData;
+    }
+
+    private float[,] generateHeights()
+    {
+        float[,] points = new float[terrainWidth, terrainLength];
+
+        for (int i = 0; i < terrainWidth; i++)
+        {
+            for (int j = 0; j < terrainLength; j++)
+            {
+                float xCoord = (float)i / terrainWidth * noiseScale;
+                float yCoord = (float)j / terrainLength * noiseScale;
+
+                points[i, j] = Mathf.PerlinNoise(xCoord, yCoord);
+            }
+        }
+
+        return points;
+    }
+    public float getTerrainLocalHeight(Vector3 position)
+    {
+        return terrainComponent.SampleHeight(position);
+    }
+}
