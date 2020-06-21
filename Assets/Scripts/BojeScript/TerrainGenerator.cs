ca02d3180912f6c2adeb5398e935ad0c3a674bdc
TheBoje <louis.leenar@gmail.com>
2020-06-21 19:44:46 +0200

    Fixed terrain generation

--- Assets/Scripts/BojeScript/TerrainGenerator.cs
+++ Assets/Scripts/BojeScript/TerrainGenerator.cs
@@ -35,27 +35,37 @@ public class TerrainGenerator : MonoBehaviour
     {
         TerrainData terrainData = oldTerrainData;
         terrainData.heightmapResolution = Mathf.Max(terrainWidth, terrainLength) + 1;
-        terrainData.size = new Vector3(terrainWidth, terrainHeight, terrainLength);
+        terrainData.size = new Vector3(nextPowerOfTwo(terrainWidth) + 1, terrainHeight, nextPowerOfTwo(terrainLength) + 1);
+        UnityEngine.Debug.Log(terrainData.size);
         terrainData.SetHeights(0, 0, generateHeights());
         return terrainData;
     }
 
     private float[,] generateHeights()
     {
-        float[,] points = new float[terrainWidth, terrainLength];
+        float[,] points = new float[nextPowerOfTwo(terrainWidth) + 1, nextPowerOfTwo(terrainLength) + 1];
 
-        for (int i = 0; i < terrainWidth; i++)
+        for (int i = 0; i < nextPowerOfTwo(terrainWidth) + 1; i++)
         {
-            for (int j = 0; j < terrainLength; j++)
+            for (int j = 0; j < nextPowerOfTwo(terrainLength) + 1; j++)
             {
                 float xCoord = (float)i / terrainWidth * noiseScale;
                 float yCoord = (float)j / terrainLength * noiseScale;
-
                 points[i, j] = Mathf.PerlinNoise(xCoord, yCoord);
             }
         }
 
         return points;
+    }
+
+    private int nextPowerOfTwo(int input)
+    {
+        int result = 1;
+        while (result < input)
+        {
+            result *= 2;
+        }
+        return result;
     }
     public float getTerrainLocalHeight(Vector3 position)
     {
