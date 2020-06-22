7a1d5ccd5bae20ade9479f21ab04826eac72a0ce
TheBoje <louis.leenar@gmail.com>
2020-06-22 19:17:23 +0200

    working perlin octaves

--- Assets/Scripts/BojeScript/TerrainGenerator.cs
+++ Assets/Scripts/BojeScript/TerrainGenerator.cs
@@ -2,9 +2,14 @@
 using UnityEditor;
 using UnityEngine.Experimental.TerrainAPI;
 using System.Drawing;
+using System.Collections.Generic;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    [SerializeField] private int octaveCount = 3;
+    [SerializeField] private float frequency = 1f;
+    [SerializeField] private float amplitude = 1f;
+
     private int terrainWidth;
     private int terrainLength;
     private float terrainHeight;
@@ -35,7 +40,7 @@ public class TerrainGenerator : MonoBehaviour
     {
         TerrainData terrainData = oldTerrainData;
         terrainData.heightmapResolution = Mathf.Max(terrainWidth, terrainLength) + 1;
-        terrainData.size = new Vector3(nextPowerOfTwo(terrainWidth) + 1, terrainHeight, nextPowerOfTwo(terrainLength) + 1);
+        terrainData.size = new Vector3(nextPowerOfTwo(terrainWidth) + 1, (int)(terrainHeight * 1.5), nextPowerOfTwo(terrainLength) + 1);
         terrainData.SetHeights(0, 0, generateHeights());
         return terrainData;
     }
@@ -43,17 +48,27 @@ public class TerrainGenerator : MonoBehaviour
     private float[,] generateHeights()
     {
         float[,] points = new float[nextPowerOfTwo(terrainWidth) + 1, nextPowerOfTwo(terrainLength) + 1];
-
+        float init_freq= frequency;
+        float init_ampl = amplitude;
         for (int i = 0; i < nextPowerOfTwo(terrainWidth) + 1; i++)
         {
             for (int j = 0; j < nextPowerOfTwo(terrainLength) + 1; j++)
             {
-                float xCoord = (float)i / terrainWidth * noiseScale;
-                float yCoord = (float)j / terrainLength * noiseScale;
-                points[i, j] = Mathf.PerlinNoise(xCoord, yCoord);
+                points[i, j] = 0f;
+                frequency = init_freq;
+                amplitude = init_ampl;
+                for (int k = 0; k < octaveCount; k++)
+                {
+                    float xCoord = (float)i / terrainWidth * noiseScale;
+                    float yCoord = (float)j / terrainLength * noiseScale;
+                    points[i, j] += Mathf.PerlinNoise(xCoord * frequency, yCoord * frequency) * amplitude;
+                    frequency *= 2f;
+                    amplitude /= 1.5f;
+                }
             }
         }
-
+        frequency = init_freq;
+        amplitude = init_ampl;
         return points;
     }
 
@@ -66,6 +81,7 @@ public class TerrainGenerator : MonoBehaviour
         }
         return result;
     }
+
     public float getTerrainLocalHeight(Vector3 position)
     {
         return terrainComponent.SampleHeight(position);
